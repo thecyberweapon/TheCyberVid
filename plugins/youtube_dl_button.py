c092f93be709ced3145ee81139b9261bd22041a9
thecyberweapon <78067399+thecyberweapon@users.noreply.github.com>
2022-04-13 17:08:01 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -87,9 +87,9 @@ async def youtube_dl_call_back(bot, update):
                 youtube_dl_url = youtube_dl_url[o:o + l]
                 progress=progress_for_pyrogram,
 progress_args=(
-                        text=Translation.DOWNLOAD_START,
-        chat_id=update.message.chat.id,
-        message_id=update.message.message_id
+          Translation.DOWNLOAD_START,
+          update.message.chat.id,
+          update.message.message_id
                     )
     user = await bot.get_me()
     mention = user["mention"]
