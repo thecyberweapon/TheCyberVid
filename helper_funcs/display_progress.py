8a9b4c5bbbce3f2de5a7a646c13112111b2c27ff
thecyberweapon <78067399+thecyberweapon@users.noreply.github.com>
2022-04-19 10:40:08 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -41,7 +41,7 @@ async def progress_for_pyrogram(
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n🚀Speed: {2}/s\n⏳ETA: {3}\n".format(
+        tmp = progress + "{0} of {1}\n\n🚀Speed: {2}/s\n\n⏳ETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
